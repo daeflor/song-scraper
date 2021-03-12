31e3d4961aa14780efdffb30e2c83fa46ad72035
daeflor <danielericflores@hotmail.com>
2021-03-12 10:04:35 -0800

    Update MessageController with minor style changes

--- Scripts/Modules/MessageController.js
+++ Scripts/Modules/MessageController.js
@@ -8,20 +8,16 @@ import * as UIController from '../AppNavigator.js';
  * @param {function} callback The function to execute when a response has been received
  */
 function sendMessageToContentScripts(message, callback) {	
-    chrome.tabs.sendMessage(
-        Model.tab.id, 
-        message, 
-        function(response) {
-            if(chrome.runtime.lastError) { //If an error occurred during the message connection, print an error
-                DebugController.logError("ERROR: An error occurred during the message connection: ");
-                console.log(chrome.runtime.lastError);
-                return;
-            }
-            else { //Otherwise, excute the provided callback function
-                callback(response); 
-            }
+    chrome.tabs.sendMessage(Model.tab.id, message, (response) => {
+        if(chrome.runtime.lastError) { //If an error occurred during the message connection, print an error
+            DebugController.logError("ERROR: An error occurred during the message connection: ");
+            console.log(chrome.runtime.lastError);
+            return;
         }
-    );
+        else { //Otherwise, excute the provided callback function
+            callback(response); 
+        }
+    });
 }
 
 /**
@@ -42,17 +38,14 @@ function handleMessageResponse(message) {
 		if (typeof message.response === 'string') { //If the response received is a string...
             Model.tracklist.title = message.response; //Update the tracklist title in the Model
             UIController.prepareLandingPage(); //Display the popup landing page
-        }
-        else {
+        } else {
             DebugController.logError("Requested tracklist title from content script, but response was not a valid string.");
         }
-	}
-    else if (message.greeting === 'GetTracklistMetadata') { 
+	} else if (message.greeting === 'GetTracklistMetadata') { 
         if (Array.isArray(message.response) === true) { //If the response received is an array...
             Model.tracklist.metadataScraped = message.response; //Update the scraped tracklist metadata in the Model
             UIController.triggerUITransition('ScrapeSuccessful'); //Transition the UI accordingly
-        }
-        else {
+        } else {
             UIController.triggerUITransition('ScrapeFailed');
             DebugController.logError("Requested tracklist metadata from content script, but response was not a valid array.");
         }
@@ -60,5 +53,8 @@ function handleMessageResponse(message) {
 }
 
 //TODO may want to restructure this similarly to new EventController
+//TODO I'm not convinced it makes a lot of sense for the MessageController to also own the logic for how to handle the response
+    //If this file were simpler and more contained, it could be a utility module (i.e. not specific to this app) that the background script could also import and leverage (e.g. to get the track count)
+        //Although the value of a simple helper for message passing is questionable since it's not that involved.
 
 export {sendMessage};
