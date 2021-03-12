603cecff393b754cd5990f7decccd4bf5631523e
daeflor <danielericflores@hotmail.com>
2021-03-12 15:24:14 -0800

    Update icon if tracklist track count has changed
    - Now load background script via html so it can be imported as a module
    - On observing a history update event, track count is retrieved from DOM
    - Then compare this to the tracklist's track count in the GPM data file
    - Set the icon depending on whether or not the track count has changed
    - Added a helper function to create image data from a local image file
    - Includes some temporary and hard-coded variables and timeouts

--- Scripts/background.js
+++ Scripts/background.js
@@ -1,4 +1,19 @@
-let rulePageIsValidTracklist = {
+//import * as Storage from './Modules/StorageManager.js';
+import * as IO from './Modules/Utilities/IO.js';
+
+//TODO could switch to using browser_action and use badge on icon to indicate if the trackcount is different than what is stored.
+    //Alternatively, could leave it as a page_action and use SetIcon to change the icon as needed
+
+console.log("Background: Running Background script.");
+
+//TODO the fact that these variables are still accessible much later on is indication that the background script isn't getting properly unloaded.
+const _iconPaths = {
+    default: 'Images/icon.png',
+    exclamation: 'Images/icon_exclamation_black.png'
+    //disabled: 'Images/icon_disabled.png',
+};
+
+const _rulePageIsValidTracklist = {
     conditions: [
         new chrome.declarativeContent.PageStateMatcher({
             pageUrl: { 
@@ -15,18 +30,145 @@ let rulePageIsValidTracklist = {
             }
         })
     ],
-    actions: [ new chrome.declarativeContent.ShowPageAction() ]
+    //actions: [ new chrome.declarativeContent.SetIcon({path:{"19" : "./icon.png"}}) ]
+    //actions: [ new chrome.declarativeContent.ShowPageAction() ]
 };
 
 //When the extenstion is installed or updated...
 chrome.runtime.onInstalled.addListener(function(details) {
 
-    //Remove all pre-existing rules to start from a clean slate and then...
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    // //Remove all pre-existing rules to start from a clean slate and then...
+    // chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
 
-        //Add the rules defined above
-        chrome.declarativeContent.onPageChanged.addRules([rulePageIsValidTracklist]);
+    //     //Add the rules defined above
+    //     //chrome.declarativeContent.onPageChanged.addRules([rulePageIsValidTracklist]);
 
-        console.log("Rules have been updated.");
+    //     console.log("Rules have been updated.");
+    // });
+
+    console.log("Background: Extension installed"); 
+
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+        
+        //createImageDataFromFile(_iconPaths.default, 96, function(imageData) {
+            let _rule = {
+                conditions: _rulePageIsValidTracklist.conditions,
+                actions: [
+                    //new chrome.declarativeContent.SetIcon({imageData: imageData}),
+                    new chrome.declarativeContent.ShowPageAction()
+                ] 
+            };
+
+            chrome.declarativeContent.onPageChanged.addRules([_rule]); 
+            console.log("Background: Rules have been updated.");     
+        //});
     });
 });
+
+// chrome.runtime.onSuspend.addListener(function() {
+//     console.log("Unloading.");
+//     //chrome.browserAction.setBadgeText({text: ""});
+// });
+
+// chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+//     //console.log(changeInfo.url);
+//     //if (changeInfo.url.includes('https://music.youtube.com/playlist')) {
+//     if (changeInfo.status === 'complete') {
+//         console.log("Tab was updated: ");
+//         console.log(tabId);
+//         console.log(changeInfo);
+//         console.log(tab);
+//     }
+// });
+
+//Note that this works because YouTube Music appears to use the History API to navigate between pages on the site
+//TODO this doesn't work when first loading the youtube site 
+   // (or refreshing the page, because this triggers before the content script can load),
+   // but it does seem to work for future site navigations.
+   // Except it doesn't work when going 'back' for some reason. Could also be a timing issue.
+   // Two possible work-arounds:
+        // Programmatic injection of code to get track count. 
+            //Then maybe save it in local storage for popup to grab later.
+        // OR setTimeout before sending the message to the content script.
+chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
+    // alert("This is my favorite website!");
+    // console.log("WebNavigation Completed")
+    //console.log(details.transitionQualifiers);
+    // console.log("ICON PATH: ");
+    // console.log(_iconPath);
+
+    setTimeout(() => {
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {greeting: 'GetTrackCount', app:'ytm'}, function(message) {
+                let tracklist = {
+                    type: 'playlist',
+                    title: undefined,
+                    currentTrackCount: message.response
+                    //storedTrackCount: undefined
+                };
+                                
+                console.log("Background: Retrieved the track count from the content script (from the DOM):" + tracklist.currentTrackCount);
+
+                chrome.tabs.sendMessage(tabs[0].id, {greeting: 'GetTracklistTitle', app:'ytm'}, (message) => {
+
+                    tracklist.title = message.response;
+
+                    const _filepath = "ExportedData/LocalStorageExport_2020-10-12-10-30PM_ThumbsUpDuplicatedAsYourLikes.txt";
+                    IO.loadTextFileViaXMLHttpRequest(_filepath, gpmLibraryObject => {
+                        console.log("Background: Retrieved GPM exported data from local file.");
+                        for (const key in gpmLibraryObject) {
+                            if (key.includes("'" + tracklist.title + "'")) {
+                                console.log("Background: Retrieved tracklist metadata from GPM exported data. Track count: " + gpmLibraryObject[key].length);
+                                gpmLibraryObject[key];
+
+                                if (tracklist.currentTrackCount !== gpmLibraryObject[key].length) {
+                                    console.log("Background: The current track count (from the DOM) is different from the stored track count.");
+                                    chrome.pageAction.setIcon({path: _iconPaths.exclamation, tabId:tabs[0].id});
+                                    // createImageDataFromFile(_iconPath, 96, (imageData) => {
+                                    //     chrome.pageAction.setIcon({imageData: imageData});  
+                                    //     //chrome.pag  
+                                    // });
+                                } else {
+                                    console.log("Background: The current track count (from the DOM) is the same as the stored track count.");
+                                    chrome.pageAction.setIcon({path: _iconPaths.default, tabId:tabs[0].id});
+                                }
+                            }
+                        }
+                    });
+
+                    // Storage.retrieveTracklistMetadata(tracklist.type, tracklist.title, (metadataArray) => {
+                    //     console.log("Background: Retrieved tracklist metadata from storage. Track count: " + metadataArray.length);
+
+                    //     if (tracklist.currentTrackCount !== metadataArray.length) {
+                    //         console.log("The current track count (from the DOM) is different from the stored track count.");
+                    //     }
+                    // });
+                });
+            });
+        });
+    }, 1000); //TODO this timeout is just temporary until a better solution can be found. On a slow device or connection, this isn't reliable. 
+}, {url: [{hostEquals : 'music.youtube.com', pathEquals: '/playlist'}]});
+
+
+// chrome.webNavigation.onCommitted.addListener(function() {
+//     alert("This is my favorite website!");
+//     console.log("WebNavigation Completed")
+// }, {url: [{urlMatches : 'https://music.youtube.com/'}]});
+
+/**
+ * Creates a canvas element, loads an image from a file, and returns the image data
+ * @param {string} path The path of the file to load
+ * @param {number} size The size of the image file (i.e. width/height in pixels). A square image is assumed.  
+ * @param {function} callback The function to execute once the image has been loaded
+ */
+function createImageDataFromFile(path, size, callback) {
+    const _canvas = document.createElement("canvas");
+    const _canvasContext = _canvas.getContext("2d");
+    const _image = new Image();
+    _image.onload = function() {
+        _canvasContext.drawImage(_image,0,0,size,size);
+        const _imageData = _canvasContext.getImageData(0,0,size,size);
+        callback(_imageData);
+    }
+    _image.src = chrome.runtime.getURL(path);
+}
