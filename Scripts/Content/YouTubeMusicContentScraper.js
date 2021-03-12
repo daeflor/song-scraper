23df2900876cf49d95ea3345c2bcbce524bcad5c
daeflor <danielericflores@hotmail.com>
2021-03-12 09:43:39 -0800

    Handle some content script messaging synchronously
    - GetTracklistTitle & GetTrackCount have been updated to be synchronous

--- Scripts/Content/YouTubeMusicContentScraper.js
+++ Scripts/Content/YouTubeMusicContentScraper.js
@@ -157,15 +157,33 @@
                 sendResponse(message);
             }
 
-            if (message.greeting == 'GetTracklistTitle') {   
-                processMessage_GetTracklistName(message.app, _onRequestComplete);
-            }
-            else if (message.greeting == 'GetTracklistMetadata') {
+            if (message.greeting === 'GetTracklistMetadata') {
                 processMessage_GetTracklistMetadata(message.app, _onRequestComplete);
             }
             
             //Return true to keep the message channel open (so the callbacks can be called asynchronously)
             return true;
+        }
+    );
+
+    chrome.runtime.onMessage.addListener(
+        function(message, sender, sendResponse) {
+            console.log(sender.tab ? 'Message received from a content script:' + sender.tab.url : 'Message received from the extension: ' + message.greeting);
+
+            if (message.greeting === 'GetTracklistTitle') {   
+                message.response = processMessage_GetTracklistName(message.app);
+                sendResponse(message);
+            }
+            else if (message.greeting === 'GetTrackCount') {   
+                //getPlaylistTrackCount(message.app, _onRequestComplete);
+                
+                // getPlaylistTrackCount(message.app, trackCount => {
+                //     message.response = trackCount;
+                //     sendResponse(message);
+                // });
+                message.response = getPlaylistTrackCount(message.app);
+                sendResponse(message);
+            }
         }
     );
 
@@ -184,16 +202,16 @@
     // }
 
     /**
-     * Gets the current tracklist name from the DOM and then executes the provided callback function
+     * Returns the current tracklist name from the DOM
      * @param {string} app the current app that the extension is running on
-     * @param {function} sendResponse The function to execute once the tracklist name has been retrieved
+     * @returns {string} the tracklist name
      */
-    function processMessage_GetTracklistName(app, callback) {
+    function processMessage_GetTracklistName(app) {
         const _tracklistNameElement = elementsInDOM.playlistName[app]();
         
-        if (_tracklistNameElement != null) {
+        if (_tracklistNameElement != null) { //TODO use a better isElement check
             console.log("Tracklist name is: " + _tracklistNameElement.textContent)
-            callback(_tracklistNameElement.textContent); //TODO this could be a return instead of a callback
+            return _tracklistNameElement.textContent;
         }
         else {
             console.log("ERROR: Received request to get the tracklist name, but it failed to be retrieved from the DOM.");
@@ -333,7 +351,7 @@
         let _scrollingTimeout = null; //Variable tracking the timeout to end the scroll & scrape process, in case no changes to the track row container element are observed for a while
         let _scrapeStartingIndex = (app == 'gpm') ? 1 : 0; //Variable to track the row index to begin each scrape with. Starts at 1 for GPM, 0 for other sites. 
         const _scrapeEndingIndexModifier = (app == 'gpm') ? -1 : 0; //Variable to track the modifier to the index to end each scrape with. Typically 0, but -1 for GPM due to how the DOM is laid out.
-
+        
         //Set up the callback function to execute once the scraped has either been successfully completed or timed out
         const _endScrape = function() {
             //Allow the user to scroll manually again
