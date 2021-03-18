7e6108ff8fe084ac49c473a33a3e9ec90b5cc6f9
daeflor <danielericflores@hotmail.com>
2021-03-18 13:10:41 -0700

    Remove unused variables & commented-out code

--- Scripts/auth/firebase-manual-auth.js
+++ Scripts/auth/firebase-manual-auth.js
@@ -4,15 +4,12 @@ import firebaseConfig from '/Scripts/Configuration/Config.js';
 import '/node_modules/firebase/firebase-app.js'; //Import the Firebase App before any other Firebase libraries
 import '/node_modules/firebase/firebase-auth.js'; //Import the Firebase Auth library
 
-let _userId = undefined;
-
 function init() {
     firebase.initializeApp(firebaseConfig); //Initialize Firebase
     firebase.auth().onAuthStateChanged(reactToEvent_AuthStateChanged);
 }
 
 function reactToEvent_AuthStateChanged(user) {
-    //console.log("Auth: The Firebase Authentication state just changed.");
     if (user === null) { //If there is no user signed into the app...
         DebugController.logInfo("Auth: A change in the Firebase Auth state was detected. Currently no user is signed in.");
         UIController.triggerUITransition('ShowAuthPrompt');
@@ -25,11 +22,7 @@ function reactToEvent_AuthStateChanged(user) {
 }
 
 export function logIn() {
-    //getAuthTokenAndAuthenticateFirebase();
-    //const _requestedScopes = ["profile"]; //"https://www.googleapis.com/auth/userinfo.email", "https://www.googleapis.com/auth/firebase"
-
-    //TODO Figure out what are the exact scopes I need to request for what I need
-    chrome.identity.getAuthToken({interactive: true/*, scopes:_requestedScopes*/}, token => {
+    chrome.identity.getAuthToken({interactive: true}, token => {
         //console.log("Retrieved cached token: " + token);
         //Generating a firebase auth credential takes either an id token (1st param) or access token (2nd param)
             //Since the token we get from chrome.identity is an access token, we have to pass that as the 2nd parameter. 
@@ -43,9 +36,8 @@ export function logOut() {
         //This will require a secure way of storing the auth token.
     firebase.auth().signOut().then(function() {
         chrome.identity.clearAllCachedAuthTokens(() => {
-            _userId = undefined;
             UIController.triggerUITransition('LogOut');
-            console.log("Cleared cached token and user ID");
+            console.log("Cleared cached token");
         });
     }).catch(function(error) {
         console.error(error);
