abb22d742065f438dec6a0608dd9f7d7a1eb637f
daeflor <danielericflores@hotmail.com>
2021-03-18 14:30:40 -0700

    Add logic to log in with Firebase SDK (but not UI)
    - Note: this logic isn't currently used, but added for future reference
    - The Firebase Auth SDK loads remote gapi scripts which MV3 disallows

--- Scripts/auth/firebase-manual-auth.js
+++ Scripts/auth/firebase-manual-auth.js
@@ -4,11 +4,18 @@ import firebaseConfig from '/Scripts/Configuration/Config.js';
 import '/node_modules/firebase/firebase-app.js'; //Import the Firebase App before any other Firebase libraries
 import '/node_modules/firebase/firebase-auth.js'; //Import the Firebase Auth library
 
+/**
+ * Sets up the Firebase context and register a listener for the Firebase Authentication state changing
+ */
 function init() {
     firebase.initializeApp(firebaseConfig); //Initialize Firebase
     firebase.auth().onAuthStateChanged(reactToEvent_AuthStateChanged);
 }
 
+/**
+ * Reacts to a detected change in the Firebase Authentication state and updates the UI accordingly
+ * @param {object} user The object containing details about the authenitcated user, if there is one. 
+ */
 function reactToEvent_AuthStateChanged(user) {
     if (user === null) { //If there is no user signed into the app...
         DebugController.logInfo("Auth: A change in the Firebase Auth state was detected. Currently no user is signed in.");
@@ -27,7 +34,7 @@ export function logIn() {
         //Generating a firebase auth credential takes either an id token (1st param) or access token (2nd param)
             //Since the token we get from chrome.identity is an access token, we have to pass that as the 2nd parameter. 
         const _credential = firebase.auth.GoogleAuthProvider.credential(null, token); 
-        firebase.auth().signInWithCredential(_credential);
+        firebase.auth().signInWithCredential(_credential); //TODO could use some error checking here (e.g. a 'catch' block)
     });
 }
 
