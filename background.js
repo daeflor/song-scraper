49f123967fdb47f31d79596a8bdcc320096dd93d
daeflor <danielericflores@hotmail.com>
2021-04-19 12:24:41 -0700

    Remove old, unnecessary code & add some comments

--- background.js
+++ background.js
@@ -59,52 +59,21 @@ chrome.runtime.onInstalled.addListener(function(details) {
 //     //chrome.browserAction.setBadgeText({text: ""});
 // });
 
-// let ignoreHistoryChange = false;
-
-// chrome.webNavigation.onCommitted.addListener(details => {
-//     console.log("On Committed Fired");
-//     console.log(details.transitionType);
-//     console.log(details.transitionQualifiers);
-//     ignoreHistoryChange = true;
-// });
-
 //Note that this works because YouTube Music appears to use the History API to navigate between pages on the site
 chrome.webNavigation.onHistoryStateUpdated.addListener(details => {
-    // if (ignoreHistoryChange === true) {
-    //     ignoreHistoryChange = false;
-    //     console.log("On History Updated Fired but it will be IGNORED THIS TIME");
-    //     console.log(details.transitionType);
-    //     console.log(details.transitionQualifiers);
-    // } else {
-        // console.log("On History Updated Fired");
-        // console.log(details.transitionType);
-        // console.log(details.transitionQualifiers);
+    if (details.url.includes('list=PL') === false) {
+        console.info("Background: Navigated to a YouTube Music page that isn't a valid tracklist. The extension icon will be disabled.");
+        chrome.action.setIcon({path: _iconPaths.disabled/*, tabId:_currentTabId*/});
+        clearCachedTracklistMetadata();
+    }
+    //TODO update this so it checks/omits other URLs, not just 'list=PL'
+    //TODO could also technically do no check and just always set the disabled icon, which would then get overwritten when the metadata gets updated/sent
+        //But I don't like that approach so much
 
-        chrome.tabs.query({active: true, currentWindow: true}, tabs => {
-            const _currentTabId = tabs[0].id;
-            if (details.url.includes('list=PL')) {
-                console.info("Background: Navigated to a YouTube Music page that is a valid playlist.");
-                // chrome.scripting.executeScript({
-                //     target: {tabId: _currentTabId}, 
-                //     files: ['/Scripts/content-scripts/scrape-tracklist-metadata.js'],}
-                // ); 
-
-                ////
-
-                // chrome.tabs.query({active: true, currentWindow: true}, tabs => {
-                //     chrome.tabs.sendMessage(tabs[0].id, {greeting: 'TestMessage'}, message => {
-                //         console.log("Background: Received message back from content script after test message was sent. Response: " + message.response);
-                //     });
-                // });
-
-            } else {
-                console.info("Background: Navigated to a YouTube Music page that isn't a valid tracklist. The extension icon will be disabled.");
-                chrome.action.setIcon({path: _iconPaths.disabled/*, tabId:_currentTabId*/});
-                clearCachedTracklistMetadata();
-            }
-        });
-    //}
+        
 }, 
+/*{url: [ {hostEquals : 'music.youtube.com', queryPrefix: '?list=PL'},
+        {hostEquals : 'music.youtube.com', queryPrefix: '?list=LM'}]}*/
 {url: [{hostEquals : 'music.youtube.com'/*, pathEquals: '/playlist'*/}]}
 );
 
@@ -114,10 +83,10 @@ chrome.runtime.onMessage.addListener(
             console.log('The current tracklist metadata was updated. New track title is "%s" and new track count is "%s".',
                 request.currentTracklistMetadata.title, request.currentTracklistMetadata.trackCount);
 
+            //TODO I could technically just put all this logic in the content script, if that helps at all 
             getTrackCountFromGPMTracklistData(request.currentTracklistMetadata.title, gpmTrackCount => {
                 if (request.currentTracklistMetadata.trackCount === gpmTrackCount) {
                     console.log("Background: The current track count (from the DOM) is the same as the stored track count.");
-                    console.log(_iconPaths);
                     chrome.action.setIcon({path: _iconPaths.default});
                 } else {
                     console.log("Background: The current track count (from the DOM) is different from the stored track count.");
