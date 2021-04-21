d4374ca9953ac41c86b6cf1783bf96a116d239c0
daeflor <danielericflores@hotmail.com>
2021-04-21 10:19:43 -0700

    Update badge text and color for 'added' & 'uploaded' lists
    - updateIcon fnc now takes a 'user-friendly' icon name instead of a path

--- background.js
+++ background.js
@@ -1,24 +1,7 @@
-
-//TODO could switch to using browser_action and use badge on icon to indicate if the trackcount is different than what is stored.
-    //Alternatively, could leave it as a page_action and use SetIcon to change the icon as needed
-        //Actually, the API has been consolidated now, so it's possible to do both!
-
-//TODO could just include this within the updateIcon function
-const _iconPaths = {
-    default: 'Images/icon.png',
-    exclamation: 'Images/icon_exclamation_black.png',
-    disabled: 'Images/icon_disabled.png',
-};
-
-// //TODO this won't work for the 'All Songs' lists
+//Note: It appears that since the transition to MV3, the declarative content rules have stopped working
+    //The icon and popup are now enabled for all pages/tabs. 
+    //Because of this, I've switched to using a different approach to only enable the icon/popup for specific pages.
 // const _conditionsForValidTracklistPage = [
-//     new chrome.declarativeContent.PageStateMatcher({
-//         pageUrl: { 
-//             hostEquals: 'play.google.com', 
-//             schemes: ['https'],
-//             pathEquals: '/music/listen'
-//         }
-//     }),
 //     new chrome.declarativeContent.PageStateMatcher({
 //         pageUrl: { 
 //             hostEquals: 'music.youtube.com', 
@@ -27,9 +10,6 @@ const _iconPaths = {
 //         }
 //     })
 // ];
-
-//TODO it seems that the page action is being shown on all pages now for some reason.
-    //Could be related to the move to manifest V3. Needs investigation.
 //When the extenstion is installed or updated...
 // chrome.runtime.onInstalled.addListener(function(details) {
 //     console.log("Background: Extension installed"); 
@@ -53,7 +33,7 @@ const _iconPaths = {
 
 // chrome.runtime.onSuspend.addListener(function() {
 //     console.log("Unloading.");
-//     //chrome.browserAction.setBadgeText({text: ""});
+//     chrome.action.setBadgeText({text: ""});
 // });
 
 //Note: this works because YouTube Music appears to use the History API to navigate between pages on the site
@@ -62,12 +42,16 @@ chrome.webNavigation.onHistoryStateUpdated.addListener(details => {
     //Update the tracklist metadata in the cache depending on the current URL
     if (details.url.includes('/library/songs') === true) {
         cacheTracklistMetadata('all', 'Added from YouTube Music'); //Cache the tracklist type and title in chrome local storage
-        updateIcon(_iconPaths.exclamation, details.tabId); //Note: since the track count isn't known prior to a complete scrape, the 'exclamation' icon is shown by default
+        updateIcon('exclamation', details.tabId); //Note: since the track count isn't known prior to a complete scrape, the 'exclamation' icon is shown by default
+        updateBadgeText("?");
+        chrome.action.setBadgeBackgroundColor({color: [255, 127, 0, 255]});
         chrome.action.setPopup({popup: 'popup.html', tabId: details.tabId}); //Allow the popup to be opened if the icon is clicked on
         //chrome.action.enable();
     } else if (details.url.includes('/library/uploaded_songs') === true) {
         cacheTracklistMetadata('uploads', 'Uploaded Songs'); //Cache the tracklist type and title in chrome local storage
-        updateIcon(_iconPaths.exclamation, details.tabId); //Note: since the track count isn't known prior to a complete scrape, the 'exclamation' icon is shown by default
+        updateIcon('exclamation', details.tabId); //Note: since the track count isn't known prior to a complete scrape, the 'exclamation' icon is shown by default
+        updateBadgeText("?"); //TODO if gonna do this, should change color too
+        chrome.action.setBadgeBackgroundColor({color: [255, 127, 0, 255]});
         chrome.action.setPopup({popup: 'popup.html', tabId: details.tabId}); //Allow the popup to be opened if the icon is clicked on
     } else if (details.url.includes('list=LM') === true) {
         //cacheTracklistMetadata('auto', 'Your Likes');
@@ -81,7 +65,7 @@ chrome.webNavigation.onHistoryStateUpdated.addListener(details => {
     } else { //Else, if the URL doesn't include any valid tracklist substrings...
         console.info("Background: Navigated to a YouTube Music page that isn't a valid tracklist. The extension icon will be disabled.");
         clearCachedTracklistMetadata(); //Clear the metadata cached in storage
-        updateIcon(_iconPaths.disabled, details.tabId); //Disable the extension icon
+        updateIcon('disabled', details.tabId); //Disable the extension icon
         updateBadgeText("", details.tabId); //Clear any badge text on the icon
         chrome.action.setPopup({popup: '', tabId: details.tabId}); //Prevent the popup from being able to be opened
     }
@@ -109,16 +93,6 @@ chrome.runtime.onMessage.addListener(
     }
 );
 
-//TODO There should be a better way to only have the popup enabled for specific tabs/pages
-// chrome.tabs.onCreated.addListener(tab => {
-//     console.log("Disabling icon for tab:");
-//     console.log(tab);
-//     chrome.action.disable(tab.id, () => {
-//         console.log("The icon should now be disabled.");
-//         //TODO i don't think the tab is fully set up at this point so the popup doesn't actually get disabled
-//     });
-// });
-
 /**
  * Compares the current and stored track counts for the current tracklist, and then updates the extension icon accordingly
  * @param {string} tracklistTitle The title of the tracklist, used to fetch the track count from storage
@@ -132,12 +106,12 @@ function compareTrackCountsAndUpdateIcon(tracklistTitle, currentTrackCount) {
         const _trackCountDelta = currentTrackCount - gpmTrackCount;
         if (_trackCountDelta === 0) {
             console.info("Background: The current track count (from the DOM) is the same as the stored track count.");
-            updateIcon(_iconPaths.default);
+            updateIcon('default');
         } else {
             console.info("Background: The current track count (from the DOM) is different from the stored track count.");
-            updateIcon(_iconPaths.exclamation);
+            updateIcon('exclamation');
             const _badgeText = (_trackCountDelta > 0) ? "+" + _trackCountDelta.toString() : _trackCountDelta.toString(); //Prefix the badge text with a "+" if the delta is positive
-            const _badgeColor = (_trackCountDelta > 0) ? [255, 127, 0, 255] : [255, 0, 0, 255]; //Set to badge color to yellow if the delta is positive, red if negative
+            const _badgeColor = (_trackCountDelta > 0) ? [255, 127, 0, 255] : [255, 0, 0, 255]; //Set to badge color to orange if the delta is positive, red if negative
             updateBadgeText(_badgeText);
             chrome.action.setBadgeBackgroundColor({color: _badgeColor});
         }
@@ -146,15 +120,22 @@ function compareTrackCountsAndUpdateIcon(tracklistTitle, currentTrackCount) {
 
 /**
  * Updates the extension icon using the image at the given path for the specified tab or the active tab, if none is specified
- * @param {string} path The path to the image file to use to update the icon
+ * @param {string} name The user-friendly name of the icon that should be show. Accepted values are: 'default', 'disabled', 'exclamation'
  * @param {number} [tabId] An optional ID to indicate the tab which should have its icon updated. If none is specified, only the active tab has its icon updated.
  */
-function updateIcon(path, tabId) {
+function updateIcon(name, tabId) {
+
+    const _iconPaths = {
+        default: 'Images/icon.png',
+        disabled: 'Images/icon_disabled.png',
+        exclamation: 'Images/icon_exclamation_black.png' //TODO would prefer a different 'friendly' name for this
+    };
+
     if (typeof tabId === 'number') {
-        chrome.action.setIcon({path: path, tabId: tabId});
+        chrome.action.setIcon({path: _iconPaths[name], tabId: tabId});
     } else {
         chrome.tabs.query( { active: true, currentWindow: true}, tabs => { 
-            chrome.action.setIcon({path: path, tabId: tabs[0].id});
+            chrome.action.setIcon({path: _iconPaths[name], tabId: tabs[0].id});
         });
     }
 }
