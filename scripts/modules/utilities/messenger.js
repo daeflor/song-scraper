05012539f1d68a27be2a720653f1be5df3e5507a
daeflor <danielericflores@hotmail.com>
2021-04-21 17:22:40 -0700

    Remove old deprecated code

--- scripts/modules/utilities/messenger.js
+++ scripts/modules/utilities/messenger.js
@@ -6,22 +6,6 @@ function init() {
     });
 }
 
-// /**
-//  * Sends a message to content scripts and then executes the provided callback once a response has been received
-//  * @param {object} messageToSend A JSON-ifiable object to send as a message
-//  * @param {function} callback The function to execute when a response has been received
-//  */
-// export function sendMessageToContentScripts(messageToSend, callback) {	
-//     chrome.tabs.sendMessage(tabId, messageToSend, messageReceived => {
-//         if (typeof chrome.runtime.lastError === 'undefined') {
-//             callback(messageReceived.response);
-//         } else console.error("Error encountered while attempting to send a message to content scripts: " + chrome.runtime.lastError.message);
-//     });
-// }
-
-
-//
-
 /**
  * Sends a message to content scripts and then executes the provided callback once a response has been received
  * @param {string} messageGreeting The greeting to use to identify the message
@@ -37,32 +21,5 @@ function init() {
         } else console.error("Error encountered while attempting to send a message to content scripts: " + chrome.runtime.lastError.message);
     });
 }
-
-//
-
-// /**
-//  * Sends a message to content scripts with the provided greeting string
-//  * @param {string} messageGreeting The greeting string used to identify the message
-//  */
-// function sendMessage(messageGreeting) {
-//     const _message = {greeting:messageGreeting}; //Create the message object to pass to the content script
-//     sendMessageToContentScripts(_message, handleMessageResponse); //Send the message
-// }
-
-// /**
-//  * Handles message responses received from content scripts
-//  * @param {object} message An object containing message data, including the greeting and response
-//  */
-// function handleMessageResponse(message) {
-// 	if (message.greeting === 'GetTracklistMetadata') { 
-//         if (Array.isArray(message.response) === true) { //If the response received is an array...
-//             Model.setScrapedTracksArray(message.response); //TODO not sure about this naming //Update the scraped tracklist metadata in the Model
-//             UIController.triggerUITransition('ScrapeSuccessful'); //Transition the UI accordingly
-//         } else {
-//             UIController.triggerUITransition('ScrapeFailed');
-//             DebugController.logError("Requested tracklist metadata from content script, but response was not a valid array.");
-//         }
-// 	}
-// }
 
 init();
