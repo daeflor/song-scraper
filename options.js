90c8e7145cbb43d2c5c9ef5ea254784a561c7f31
daeflor <danielericflores@hotmail.com>
2021-05-16 01:18:17 -0700

    Check the correct preference when the options screen opens
    Before, the top preference in the list was always checked,
    even if the preference set by the user (and in storage) was different.

--- options.js
+++ options.js
@@ -1,5 +1,13 @@
 import * as chromeStorage from '../scripts/modules/utilities/chrome-storage-promises.js'
-console.info("Options page loaded");
+import * as appStorage from './scripts/modules/StorageManager.js';
+
+async function updatePreferencesScreenUI() {
+    const comparisonMethod = await appStorage.getPreferencesFromChromeSyncStorage('Comparison Method');
+    const optionToCheck = document.getElementById(comparisonMethod);
+    if (optionToCheck instanceof Element) {
+        optionToCheck.checked = true;
+    }
+}
 
 /**
  * Updates the value for the given user preference in chrome storage
@@ -7,27 +15,14 @@ console.info("Options page loaded");
  * @param {string} value The value to set the preference to
  */
 async function updatePreferencesInChromeSyncStorage(preference, value) {
-    const preferencesObject = await getPreferencesFromChromeSyncStorage();
+    const preferencesObject = await appStorage.getPreferencesFromChromeSyncStorage();
     console.info("Current " + preference + " preference in storage is: " + preferencesObject[preference] + ". New value will be: " + value);
     preferencesObject[preference] = value;
     chromeStorage.set('sync', {preferences:preferencesObject});
+    //TODO currently, when the preference is changed, the icon isn't updated to reflect the new (possibly different) track count delta. This doesn't seem like a big deal.
 }
 
 document.getElementById('alwaysYTM').addEventListener('change', event => updatePreferencesInChromeSyncStorage('Comparison Method', event.target.value));
 document.getElementById('preferYTM').addEventListener('change', event => updatePreferencesInChromeSyncStorage('Comparison Method', event.target.value));
 document.getElementById('alwaysGPM').addEventListener('change', event => updatePreferencesInChromeSyncStorage('Comparison Method', event.target.value));
-
-//TODO Duplicated due to Chromium Bug 824647
-//TODO this would be good to put in a module that both background and options scripts can access, once Chrome 91 releases.
-/**
- * Returns the user's preferences object, or a particular preference value if specified
- * @param {string} [preference] An optional preference to specify, if only one value is desired
- * @returns {Promise} A promise with either an object containing all the user's preferences, or the value of a single preference, if specified
- */
- async function getPreferencesFromChromeSyncStorage(preference) {
-    const preferencesKey = 'preferences';
-    const storageItems = await chromeStorage.getKeyValuePairs('sync', preferencesKey);
-    return (typeof preference === 'undefined')
-    ? storageItems[preferencesKey]
-    : storageItems[preferencesKey]?.[preference];
-}
+updatePreferencesScreenUI();
