8f2f879e1c1990350bc878b0299e8f8b8e008682
daeflor <danielericflores@hotmail.com>
2022-10-23 13:14:35 +0200

    Update options-storage to use getValueAtKey instead of getKeyValuePairs
    Also, it now uses a const STORAGE_ITEM_KEY that is defined a single time

--- scripts/options/options-storage.js
+++ scripts/options/options-storage.js
@@ -1,14 +1,16 @@
 import * as chromeStorage from '../modules/utilities/chrome-storage-promises.js';
 
+const STORAGE_ITEM_KEY = 'preferences';
+
 /**
  * Returns the user's preferences object, or a particular preference value if specified
  * @param {string} [preference] An optional preference to specify, if only one value is desired
  * @returns {Promise} A promise with either an object containing all the user's preferences, or the value of a single preference, if specified
  */
- export default async function getPreferencesFromChromeSyncStorage(preference) {
-    const preferencesKey = 'preferences';
-    const storageItems = await chromeStorage.getKeyValuePairs('sync', preferencesKey);
-    return (typeof preference === 'undefined')
-    ? storageItems[preferencesKey]
-    : storageItems[preferencesKey]?.[preference];
+ export default async function getPreferencesFromChromeSyncStorage(preferenceKey) {
+    const preferencesStorageItem = await chromeStorage.getValueAtKey('sync', STORAGE_ITEM_KEY);
+    console.log(preferencesStorageItem);
+    return (typeof preferenceKey === 'undefined')
+    ? preferencesStorageItem
+    : preferencesStorageItem?.[preferenceKey];
 }
