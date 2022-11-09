15c164d6424591d30c2815506bd9aa67d0458241
daeflor <danielericflores@hotmail.com>
2022-11-09 13:31:35 +0100

    Add asserts to some of the Chrome Storage tests
    Also added increased messaging & ability to run multiple tests in series

--- tests/chrome-storage-tests.js
+++ tests/chrome-storage-tests.js
@@ -1,22 +1,62 @@
 import { ChromeStorageAccessor } from '../scripts/storage/chrome-storage.js';
 
-export async function testChromeStorageAccessor(test) {
+async function testChromeStorageAccessor(testNumber) {
     let testAccessor = undefined;
-    switch(test) {
+    let error = undefined;
+    let expectedErrorMessageSnippet = undefined;
+    const assertMessage = "Chrome Storage Test #%s Failed";
+    switch(testNumber) {
     case 0: // Storage Area incorrect (Should result in error)
-        testAccessor = new ChromeStorageAccessor('xsync', 'testItem');
+        
+        try {
+            testAccessor = new ChromeStorageAccessor('xsync', 'testItem');
+        } catch (caughtError) {
+            error = caughtError;
+
+            // if (caughtError.name === 'RangeError' && caughtError.message.includes("An invalid storage area was provided for accessing Chrome storage") === true) {
+            //     error = caughtError;
+            // }
+        }
+        console.assert(error?.name === 'RangeError' && error?.message.includes("An invalid storage area was provided for accessing Chrome storage") === true, "Chrome Storage Test #0 Failed");
+        
+        //console.assert(typeof error !== 'undefined', "Chrome Storage Test #0 Failed");
+        
+        console.info("Test #%s Completed", testNumber);
         return;
     case 1: // Get Property when Storage Item doesn't exist (Should result in error)
-        testAccessor = new ChromeStorageAccessor('sync', 'xtestItem');
-        await testAccessor.getProperty('testKey');
+        try {
+            testAccessor = new ChromeStorageAccessor('sync', 'xtestItem');
+            await testAccessor.getProperty('testKey');
+        } catch (caughtError) {
+            error = caughtError;
+        }
+
+        expectedErrorMessageSnippet = "there is no item associated with the key provided";
+        console.assert(error?.message.includes(expectedErrorMessageSnippet) === true, assertMessage, testNumber);
+        console.info("Test #%s Completed", testNumber);
         return;
     case 2: // Set Property without passing key (Should result in error)
-        testAccessor = new ChromeStorageAccessor('sync', 'testItem');
-        await testAccessor.setProperty();
+        try {
+            testAccessor = new ChromeStorageAccessor('sync', 'testItem');
+            await testAccessor.setProperty();
+        } catch (caughtError) {
+            error = caughtError;
+        }
+
+        expectedErrorMessageSnippet = "an invalid property key or value was provided";
+        console.assert(error?.message.includes(expectedErrorMessageSnippet) === true, assertMessage, testNumber);
+        console.info("Test #%s Completed", testNumber);
         return;
-    case 3: // Set Property when Storage Item doesn't exist
+    case 3: // Set Property when Storage Item doesn't exist (Should add a new storage item with the given key and property kvp)
+        await chrome.storage.sync.remove('testItem');
         testAccessor = new ChromeStorageAccessor('sync', 'testItem');
         await testAccessor.setProperty('testPropertyKey', 'testPropertyValue');
+        const chromeStorageItems = await chrome.storage.sync.get(null);
+
+        //console.assert(Object.hasOwn(chromeStorageItems.testItem, 'testPropertyKey') === true, assertMessage, testNumber);
+        //console.assert(Object.hasOwn(chromeStorageItems, 'testItem') === true && Object.hasOwn(chromeStorageItems.testItem, 'testPropertyKey') === true, assertMessage, testNumber);
+        console.assert(chromeStorageItems.testItem?.testPropertyKey === 'testPropertyValue', assertMessage, testNumber);
+        console.info("Test #%s Completed", testNumber);
         return;
     case 4: // Get Property without passing key (Should result in error)
         testAccessor = new ChromeStorageAccessor('sync', 'testItem');
@@ -61,4 +101,19 @@ export async function testChromeStorageAccessor(test) {
     }
 }
 
-testChromeStorageAccessor(9);
+chrome.alarms.create({ when: Date.now() + 500 });
+
+chrome.alarms.onAlarm.addListener(() => {
+    runAllTests();
+});
+
+export async function runAllTests() {
+    console.group();
+    console.info("Running Chrome Storage Tests");
+    await testChromeStorageAccessor(0);
+    await testChromeStorageAccessor(1);
+    await testChromeStorageAccessor(2);
+    await testChromeStorageAccessor(3);
+    console.info("Tests Complete");
+    console.groupEnd();
+}
